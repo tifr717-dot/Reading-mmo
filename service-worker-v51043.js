04fdff2780425f0ed85f986a0445f8bb2a64afc4
tifr717-dot <tifr717@gmail.com>
2026-09-05 20:26:30 -0500

    Refresh Journal layout cache v5.10.43h

--- service-worker-v51043.js
+++ service-worker-v51043.js
@@ -1,5 +1,5 @@
-const CACHE='reading-mmo-v5.10.43-storybook-journal-g';
-const FORCE_VERSION='51043g';
+const CACHE='reading-mmo-v5.10.43-storybook-journal-h';
+const FORCE_VERSION='51043h';
 const CORE=[
  './','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
  './nav-home.png','./nav-me.png','./nav-play.png','./nav-quests.png','./nav-read.png',
