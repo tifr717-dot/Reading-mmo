184db83732bfee9860103113453ca3613ac53eb9
tifr717-dot <tifr717@gmail.com>
2026-09-08 21:05:44 -0500

    Refresh Journal rebuild cache v5.10.43r

--- service-worker-v51043.js
+++ service-worker-v51043.js
@@ -1,5 +1,5 @@
-const CACHE='reading-mmo-v5.10.43-storybook-journal-q';
-const FORCE_VERSION='51043q';
+const CACHE='reading-mmo-v5.10.43-storybook-journal-r';
+const FORCE_VERSION='51043r';
 const CORE=[
  './','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
  './nav-home.png','./nav-me.png','./nav-play.png','./nav-quests.png','./nav-read.png',
